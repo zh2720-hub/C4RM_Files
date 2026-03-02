0bc917ff0a76ab540741061228e47df9fc1d9219
zh2720-hub <zh2720@columbia.edu>
2026-03-02 15:32:50 -0500

    Update BondPrice_File.py

--- Python_HW2_StarterFiles/BondPrice_File.py
+++ Python_HW2_StarterFiles/BondPrice_File.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+    n = int(m * ppy)
+    t = np.arange(1, n + 1)
+
+    coupon = face * couponRate / ppy
+    r = y / ppy
+
+    cashflows = np.full(n, coupon, dtype=float)
+    cashflows[-1] += face
+
+    discount_factors = (1 + r) ** t
+    price = np.sum(cashflows / discount_factors)
+
+    return price
